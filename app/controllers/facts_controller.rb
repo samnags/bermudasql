64be4a754c19c3c7b1ffb7a8f6ac6bf9c2c77603
samnags <sam.nagourney@gmail.com>
2016-06-26 14:05:38 -0400

    facts page

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,2 +1,6 @@
 class FactsController < ApplicationController
+
+  def facts
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/faq' => 'faq#faq'
   get '/index' => 'home#index'
   get '/contact' => 'contact#contact'
+  get '/facts' => 'facts#facts'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
